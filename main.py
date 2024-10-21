4870b0a981335e78597ff81902fab992246e7ea6
Phuong Ngo <phuongngovan2003@gmail.com>
2024-10-22 00:16:30 +0700

    Add log message

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ SAMPLE_FILE = 'output/samples.txt'
 SENTENCE_FILE = 'input/sentences.txt'
 PARSE_FILE = 'output/parse-results.txt'
 
+print("Loading grammar...")
 grammar = vacatio.context_free_grammar()
+print("Grammar loaded")
 
 # dump grammar
+print("Dumping grammar...")
 grammar.save(GRAMMAR_FILE)
+print("Grammar saved to", GRAMMAR_FILE)
 
 # generate samples
+print("Generating samples...")
 grammar.generate(max_length=10, max_samples=10000, filename=SAMPLE_FILE)
+print("Samples saved to", SAMPLE_FILE)
 
 # parse sentences
+print("Parsing sentences...")
 with open(SENTENCE_FILE, 'r') as f:
     sents = f.read().splitlines()
 
 grammar.parse_all(sents, filename=PARSE_FILE)
+print("Parse results saved to", PARSE_FILE)
